d91e11f7fea86aebfbc5ce6eb03ccd9cb89ebe3e
Yanbo Huang <yanbo.huang@intel.com>
2023-09-22 10:16:36 +0800

    Revert "IpmiFeaturePkg: Refine code to avoid warning report"
    This reverts commit 2009222701e9e1e62693c3d7ce5e202751cf405d.
    The upstream SHA-ID is cf8a7168c3e8b56b41e09097660c5a6048f1b8ef.
    Reverting this patch in downstream until issue fix.
    
    Change-Id: Ied437ecfc4a4bcb10a4f1fd156cbec29dccad9be
    Signed-off-by: Yanbo Huang <yanbo.huang@intel.com>

--- Features/Intel/OutOfBandManagement/IpmiFeaturePkg/GenericIpmi/Pei/PeiGenericIpmi.c
+++ Features/Intel/OutOfBandManagement/IpmiFeaturePkg/GenericIpmi/Pei/PeiGenericIpmi.c
@@ -343,8 +343,8 @@ PeiInitializeIpmiKcsPhysicalLayer (
   // Check interface data initialized successfully else register notify protocol.
   for (Index = SysInterfaceKcs; Index < SysInterfaceMax; Index++) {
     switch (Index) {
-      case SysInterfaceKcs:
-        if (FixedPcdGet8 (PcdKcsInterfaceSupport) == 1) {
+    if (FixedPcdGet8 (PcdKcsInterfaceSupport) == 1) {
+        case SysInterfaceKcs:
           if ((mIpmiInstance->BmcStatus != BMC_HARDFAIL) && (mIpmiInstance->BmcStatus != BMC_UPDATE_IN_PROGRESS)) {
             BMC_INTERFACE_STATUS  BmcStatus;
             mIpmiInstance->IpmiTransport2Ppi.Interface.KcsInterfaceState = IpmiInterfaceInitialized;
@@ -359,38 +359,42 @@ PeiInitializeIpmiKcsPhysicalLayer (
               mIpmiInstance->IpmiTransport2Ppi.Interface.KcsInterfaceState = IpmiInterfaceInitError;
             }
           }
-        }
-        break;
 
-      case SysInterfaceBt:
-        if (FixedPcdGet8 (PcdBtInterfaceSupport) == 1) {
+          break;
+    }
+
+    if (FixedPcdGet8 (PcdBtInterfaceSupport) == 1) {
+        case SysInterfaceBt:
           if (mIpmiInstance->IpmiTransport2Ppi.Interface.Bt.InterfaceState == IpmiInterfaceInitialized) {
             InterfaceState = IpmiInterfaceInitialized;
           }
-        }
-        break;
 
-      case SysInterfaceSsif:
-        if (FixedPcdGet8 (PcdSsifInterfaceSupport) == 1) {
+          break;
+    }
+
+    if (FixedPcdGet8 (PcdSsifInterfaceSupport) == 1) {
+        case SysInterfaceSsif:
           if (mIpmiInstance->IpmiTransport2Ppi.Interface.Ssif.InterfaceState == IpmiInterfaceInitialized) {
             InterfaceState = IpmiInterfaceInitialized;
           } else if (mIpmiInstance->IpmiTransport2Ppi.Interface.Ssif.InterfaceState == IpmiInterfaceInitError) {
             // Register protocol notify for SMBUS Protocol.
             Status = RegisterPpiCallback (PeiServices, &mIpmiInstance->IpmiTransport2Ppi.Interface.Ssif.SsifInterfaceApiGuid);
           }
-        }
-        break;
 
-      case SysInterfaceIpmb:
-        if (FixedPcdGet8 (PcdIpmbInterfaceSupport) == 1) {
+          break;
+    }
+
+    if (FixedPcdGet8 (PcdIpmbInterfaceSupport) == 1) {
+        case SysInterfaceIpmb:
           if (mIpmiInstance->IpmiTransport2Ppi.Interface.Ipmb.InterfaceState == IpmiInterfaceInitialized) {
             InterfaceState = IpmiInterfaceInitialized;
           } else if (mIpmiInstance->IpmiTransport2Ppi.Interface.Ipmb.InterfaceState == IpmiInterfaceInitError) {
             // Register protocol notify for SMBUS Protocol.
             Status = RegisterPpiCallback (PeiServices, &mIpmiInstance->IpmiTransport2Ppi.Interface.Ipmb.IpmbInterfaceApiGuid);
           }
-        }
-        break;
+
+          break;
+    }
 
       default:
         break;
